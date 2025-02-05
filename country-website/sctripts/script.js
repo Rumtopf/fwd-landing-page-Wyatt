d3896418c5569acdcf4a947b200d6c2b8c5ead5e
Wyatt <wyattspratt@hotmail.com>
2025-02-04 21:09:34 -0800

    added detail about nav ham menu

---
+++ country-website/sctripts/script.js
@@ -0,0 +1,7 @@
+// Buger Nav
+const button = document.getElementById('burger-button');
+const links = document.getElementById('main-nav-list');
+
+button.addEventListener('click', function(){
+links.classList.toggle('toggled');
+ })
